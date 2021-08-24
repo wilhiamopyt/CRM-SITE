ff599f21f4c126db0896e0fc8d25b10142f7d141
wilhiamopyt <william.growth.opyt@gmail.com>
2021-08-24 18:05:16 -0300

    Atualização 24-08-21
    Melhorias na Faqs implementação de codigo js

--- faqs.js
+++ faqs.js
@@ -1,19 +1,26 @@
-function pesquisa(){
-    var input,filtro,menu,menuItens,links
-    input=document.getElementById("textinput").value;
+function pesquisa() {
+    var input, filtro, menu, menuItens, links, teste
+    input = document.getElementById("textinput").value;
     console.log(input)
-    filtro=input.toUpperCase();
-     menu=document.getElementById("menu");
-     menuItens=document.getElementsByTagName("li");
-    for(var i=0;i<menuItens.length;i++){
-        var links =menuItens[i].getElementsByTagName("p")[0];
-        if(links.innerHTML.toUpperCase().indexOf(filtro)>=0){
-            menuItens[i].style.display="";
+    filtro = input.toUpperCase();
+    menu = document.getElementById("menu");
+    menuItens = document.getElementsByTagName("li");
+    
+    for (var i = 0; i < menuItens.length; i++) {
+        var links = menuItens[i].getElementsByTagName("p")[0];
+        
+        if (links.innerHTML.toUpperCase().indexOf(filtro)>0) {
+            menuItens[i].style.display = "";
+            menuItens[2].style.display="none";
+            
 
-        }else{
-            menuItens[i].style.display="none";
+
+
+        } else {
+            menuItens[i].style.display = "none";
+            menuItens[2].style.display="";
+            
         }
+
     }
-
-
 }
